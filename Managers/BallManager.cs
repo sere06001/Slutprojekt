534e70a756b45c5ae7ad1a5281fb1309e4867f68
sere06001 <sere06001@utb.vaxjo.se>
2025-03-20 13:25:51 +0100

    Move ball stuff into BallManager.cs

---
+++ Managers/BallManager.cs
@@ -0,0 +1,51 @@
+namespace Slutprojekt;
+public class BallManager
+{
+    private readonly List<Ball> balls = [];
+
+    public BallManager()
+    {
+        balls.Add(new());
+        balls.Add(new());
+    }
+
+    private void CheckCollisions()
+    {
+        for (int i = 0; i < balls.Count - 1; i++)
+        {
+            for (int j = i + 1; j < balls.Count; j++)
+            {
+                if ((balls[i].Position - balls[j].Position).Length() < (balls[i].Origin.X + balls[j].Origin.X))
+                {
+                    ResolveCollision(balls[i], balls[j]);
+                    break;
+                }
+            }
+        }
+    }
+
+    private void ResolveCollision(Ball b1, Ball b2)
+    {
+        var dir = Vector2.Normalize(b1.Position - b2.Position);
+        b1.Direction = dir;
+        b2.Direction = -dir;
+    }
+
+    public void Update()
+    {
+        foreach (var balls in balls)
+        {
+            balls.Update();
+        }
+
+        CheckCollisions();
+    }
+
+    public void Draw()
+    {
+        foreach (var balls in balls)
+        {
+            balls.Draw();
+        }
+    }
+}
--- Managers/GameManager.cs
+++ Managers/GameManager.cs
@@ -1,53 +1,21 @@
 namespace Slutprojekt;
 public class GameManager
 {
-    private readonly List<Ball> balls = [];
+    private BallManager ballManager = new();
 
     public GameManager()
     {
-        for (int i = 0; i < 10; i++)
-        {
-            balls.Add(new());
-        }
+
     }
 
-    private void CheckCollisions()
-    {
-        for (int i = 0; i < balls.Count - 1; i++)
-        {
-            for (int j = i + 1; j < balls.Count; j++)
-            {
-                if ((balls[i].Position - balls[j].Position).Length() < (balls[i].Origin.X + balls[j].Origin.X))
-                {
-                    ResolveCollision(balls[i], balls[j]);
-                    break;
-                }
-            }
-        }
-    }
-
-    private void ResolveCollision(Ball b1, Ball b2)
-    {
-        var dir = Vector2.Normalize(b1.Position - b2.Position);
-        b1.Direction = dir;
-        b2.Direction = -dir;
-    }
 
     public void Update()
     {
-        foreach (var balls in balls)
-        {
-            balls.Update();
-        }
-
-        CheckCollisions();
+        ballManager.Update();
     }
 
     public void Draw()
     {
-        foreach (var balls in balls)
-        {
-            balls.Draw();
-        }
+        ballManager.Draw();
     }
 }
