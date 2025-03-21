bd02d73864881237beb488800c3dd5b14c28f071
sere06001 <sere06001@utb.vaxjo.se>
2025-03-21 16:23:35 +0100

    Update TODO.txt

--- obj/Debug/net8.0-windows/Slutprojekt.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Slutprojekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Slutprojekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83014662db959f9101da99262e52e27a451aa2b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97b2243958f28069b92b37c9c82b657e26229282")]
 [assembly: System.Reflection.AssemblyProductAttribute("Slutprojekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Slutprojekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
