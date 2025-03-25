8b4ae373c63d266935e1deaaead9f3a3f97ed6f6
sere06001 <sere06001@utb.vaxjo.se>
2025-03-25 23:37:08 +0100

    Add comments, update TODO.txt

--- Custom stuff/Managers/GameManager.cs
+++ Custom stuff/Managers/GameManager.cs
@@ -6,6 +6,8 @@ public class GameManager
 
     public GameManager()
     {
+        //Change to LevelGenerator.Update() later, currentLevel is just for testing purposes
+
         //currentLevel = new LevelCircle(ballManager, 100f, 10, Globals.Bounds.X/2, Globals.Bounds.Y/2, false); //Just for testing
         currentLevel = new LevelGrid(ballManager, 5, 5, Globals.BrickBlue.Width, Globals.Bounds.X/2, Globals.Bounds.Y/2, true, 0); //Just for testing
     }
@@ -13,9 +15,9 @@ public class GameManager
     public void Update()
     {
         ballManager.Update();
-        currentLevel.Update();
+        currentLevel.Update(); //Also for testing, change to LevelGenerator.Update() which will handle the main level system to keep GameManager clean
     
-        if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+        if (Mouse.GetState().LeftButton == ButtonState.Pressed) //Temporary ball spawner for testing, make it spawn 1 ball per click for more fine tuned testing
         {
             Vector2 mousePosition = Mouse.GetState().Position.ToVector2();
     
