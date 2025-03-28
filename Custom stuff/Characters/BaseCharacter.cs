78d021cd5e9b6e956aab5f6de33f7718a3d1baae
sere06001 <143172163+sere06001@users.noreply.github.com>
2025-03-28 03:03:42 +0100

    Update BaseCharacter.cs

--- Custom stuff/Characters/BaseCharacter.cs
+++ Custom stuff/Characters/BaseCharacter.cs
@@ -1,8 +1,11 @@
 namespace Slutprojekt;
 public abstract class BaseCharacter
 {
-    protected abstract BasePowerup Powerup { get; set; }
-    protected abstract Texture texture { get; set; }
+    protected virtual BasePowerup Powerup { get; set; }
+    protected virtual Texture texture { get; set; }
     protected virtual Vector2 Position { get; set; }
-    public abstract void Draw();
+    public virtual void Draw()
+    {
+
+    }
 }
