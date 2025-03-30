1dade4bff0d32aac035c5ac27df24198372004af
sere06001 <sere06001@utb.vaxjo.se>
2025-03-30 01:35:11 +0100

    Update comment in Player.cs

--- Custom stuff/Player.cs
+++ Custom stuff/Player.cs
@@ -1,7 +1,7 @@
 namespace Slutprojekt;
 public class Player
 {
-    public BasePowerup Powerup { get; set; } //Assign powerup here so stuff can call player.Powerup likewise with character etc
+    public BasePowerup Powerup { get; set; } //Assign powerup here so stuff can call player.Powerup likewise with character etc to minimize clutter in GameManager.cs
     public BaseCharacter Character { get; set; }
 
 }
