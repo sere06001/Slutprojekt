2846b38b5b72c53ce0358d3a2a03425e287fbad6
sere06001 <sere06001@utb.vaxjo.se>
2025-04-03 19:40:40 +0200

    Update todo.txt

--- Custom stuff/Level/LevelGenerator.cs
+++ Custom stuff/Level/LevelGenerator.cs
@@ -6,6 +6,7 @@ public class LevelGenerator
     public Player player;
     public BallManager ballManager;
     private LevelCircle levelCircle;
+    private LevelCircle levelCircle2;
     private LevelGrid levelGrid;
     public LevelGenerator(BallManager ballmngr, Player plyr)
     {
@@ -15,6 +16,7 @@ public class LevelGenerator
     public void Face()
     {
         levelCircle = new(ballManager, player, 100f, 10, 500, 500, false);
+        levelCircle2 = new(ballManager, player, 100f, 10, 500, 500, false);
         levelGrid = new(ballManager, player, 1, 10, 100, 250, 250, true);
     }
     public void Init()
@@ -24,11 +26,13 @@ public class LevelGenerator
     public void Update()
     {
         levelCircle.Update();
+        levelCircle2.Update();
         levelGrid.Update();
     }
     public void Draw()
     {
         levelCircle.Draw();
+        levelCircle2.Draw();
         levelGrid.Draw();
     }
 }
