40903a9d5706014f5bb1e55013e94538a987ac76
sere06001 <sere06001@utb.vaxjo.se>
2025-04-03 20:22:09 +0200

    Remove these unused

--- Custom stuff/Level/LevelGenerator.cs
+++ Custom stuff/Level/LevelGenerator.cs
@@ -4,9 +4,6 @@ public class LevelGenerator
     public Player player;
     public BallManager ballManager;
     private LevelCombiner levelCombiner;
-    private LevelCircle levelCircle;
-    private LevelCircle levelCircle2;
-    private LevelCircle levelCircle3;
     public LevelGenerator(BallManager ballmngr, Player plyr, LevelCombiner lvlcombiner)
     {
         ballManager = ballmngr;
